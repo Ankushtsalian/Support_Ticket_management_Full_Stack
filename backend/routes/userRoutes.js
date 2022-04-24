3de0d107a081950a168f8f2f8d70b7191b44adcc
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-24 13:17:39 +0530

    created ticket route and  contoller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const router = express.Router();
 const {
   registerUser,
   loginUser,
@@ -8,12 +8,10 @@ const {
 
 const { protect } = require("../middleware/authMiddleware");
 
-const router = express.Router();
-
 router.post("/", registerUser);
 
 router.post("/login", loginUser);
 
-// router.get("/me", protect, getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;
