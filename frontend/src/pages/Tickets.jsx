66ed59394880cd4a4608372ab71bb229285e15f0
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-28 12:23:24 +0530

    creted getTicket in ticket reducer with new route to view ticket from home page using /tickets

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 const Tickets = () => {
   const { isLoading, isSuccess, tickets } = useSelector(
@@ -23,10 +24,23 @@ const Tickets = () => {
   if (isLoading) {
     return <Spinner />;
   }
+
   return (
-    <div>
-      <BackButton url="/" /> Tickets
-    </div>
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 };
 
